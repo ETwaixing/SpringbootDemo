04e051f8fba806f7899459a9501ae31ab5d1b972
waixing <waixing@yonglang.co>
2017-04-07 17:56:43 +0800

    优化数据库连接池

--- src/main/java/com/waixing/Application.java
+++ src/main/java/com/waixing/Application.java
@@ -1,9 +1,6 @@
 package com.waixing;
 
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientOptions;
-import com.mongodb.MongoClientURI;
-import com.mongodb.WriteConcern;
+import com.mongodb.*;
 import com.waixing.config.SpringDataMongodbConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -30,12 +27,12 @@ import java.net.UnknownHostException;
 public class Application implements CommandLineRunner{
     @Autowired
     private SpringDataMongodbConfig springDataMongodbConfig;
-    @Autowired
-    private MongoTemplate mongoTemplate;
-    @Autowired
-    private MongoClient mongoClient;
-    @Autowired
-    private MongoClientURI mongoClientURI;
+//    @Autowired
+//    private MongoTemplate mongoTemplate;
+//    @Autowired
+//    private MongoClient mongoClient;
+//    @Autowired
+//    private MongoClientURI mongoClientURI;
 
     public static void main(String[] args){
         SpringApplication.run(Application.class,args);
@@ -57,18 +54,25 @@ public class Application implements CommandLineRunner{
             container.addErrorPages(error404Page, error500Page);
         });
     }
-
+    //配置数据库源
     @Bean(name = "mongoClientURI")
     @Primary
     public MongoClientURI mongoClientURI() throws UnknownHostException {
         MongoClientOptions.Builder builder = MongoClientOptions.builder().connectionsPerHost(10).writeConcern(WriteConcern.ACKNOWLEDGED);
         return new MongoClientURI(springDataMongodbConfig.getUri(), builder);
     }
-
     @Bean(name = "mongoClient")
     @Primary
     public MongoClient mongoClient(@Autowired MongoClientURI clientURI) throws UnknownHostException {
         return new MongoClient(clientURI);
+    }
+    @SuppressWarnings("deprecation")
+    @Bean(name="template")
+    @Primary
+    public MongoTemplate mongoTemplate(@Autowired MongoClientURI clientURI) throws UnknownHostException{
+        MongoURI mongoURI = new MongoURI(clientURI);
+        Mongo mongo = new Mongo(mongoURI);
+        return new MongoTemplate(mongo, mongoURI.getDatabase());
     }
 
 }
